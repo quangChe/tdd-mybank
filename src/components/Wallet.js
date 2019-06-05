a827ac0f7255c5609db882b2cd55a2c81413b239
quangChe <quang.n.che@gmail.com>
2019-06-05 16:18:51 -0700

    Wallet Input and State
    Added tests for Wallet component to check for an input and that any user
    behavior on this input will also modify the 'amount' state of the Wallet
    component. Built out this input and all functionality to pass tests.

--- src/components/Wallet.js
+++ src/components/Wallet.js
@@ -2,11 +2,24 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 export class Wallet extends React.Component {
+  state = {
+    amount: 0,
+  }
+
+  changeAmount = (e) => {
+    this.setState({ amount: parseInt(e.target.value, 10) });
+  }
+
   render() {
-    const { balance } = this.props
+    const { balance } = this.props;
+    
     return (
       <div>
         <h3 className="balance">Wallet balance: { balance }</h3>
+        <br/>
+        <input 
+          onChange={this.changeAmount}
+          className="wallet-input"/>
       </div>
     )
   }
--- src/components/Wallet.test.js
+++ src/components/Wallet.test.js
@@ -12,4 +12,20 @@ describe('Wallet', () => {
   it('correctly displays balance from props', () => {
     expect(wallet.find('.balance').text()).toEqual('Wallet balance: 20')
   })
+
+  it('contains an input to enter deposit or withdraw amount', () => {
+    expect(wallet.find('.wallet-input').exists()).toBe(true);
+  })
+
+  describe('when the user types into Wallet input', () => {
+    const INPUT_VALUE = '1000';
+
+    beforeEach(() => 
+      wallet.find('.wallet-input').simulate('change', { target: { value: INPUT_VALUE }})
+    )
+
+    it('changes the `amount` state of the Wallet', () => {
+      expect(wallet.state().amount).toBe(parseInt(INPUT_VALUE, 10));
+    })
+  })
 })
