d5f639b263f57b6fabd10cca5488a905eb7fac76
quangChe <quang.n.che@gmail.com>
2019-06-05 14:16:27 -0700

    Rendered Wallet and tested snapshot on App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Wallet from './Wallet';
 
 class App extends React.Component {
   render () {
     return (
     <>
       <h2>My Bank</h2>
+      <Wallet/>
     </>
     );
   }
