b8d05b058f4ec878d3f389e10e2071f0067103e5
quangChe <quang.n.che@gmail.com>
2019-06-05 13:55:19 -0700

    Wallet Component with Test
    Added a snapshot test for Wallet component and a basic Wallet component
    that passes this test.

---
+++ src/components/Wallet.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Wallet extends React.Component {
+  render() {
+    return (
+      <div>
+        <h3>Wallet balance</h3>
+      </div>
+    )
+  }
+}
+
+export default Wallet;
