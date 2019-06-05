d118b1deceed6337ea0764dc3a8994b963871fd7
quangChe <quang.n.che@gmail.com>
2019-06-05 13:39:45 -0700

    Wired up Provider and Redux store to app

--- src/index.js
+++ src/index.js
@@ -4,7 +4,18 @@ import './index.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { createStore } from 'redux';
+import rootReducer from './reducers/my-balance';
+import { Provider } from 'react-redux';
+
+const store = createStore(rootReducer);
+const MY_BANK_APP = (
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
+
+ReactDOM.render(MY_BANK_APP, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
