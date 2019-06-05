e6206ac972d5c5d5d697800c62726dc2029ec102
quangChe <quang.n.che@gmail.com>
2019-06-05 15:19:34 -0700

    Deposit Reducer and Action Creator
    Added tests for a 'deposit' action creator and reducer, built out this
    action creator and reducer, and completed testing for them.

--- src/actions/my-balance.test.js
+++ src/actions/my-balance.test.js
@@ -1,10 +1,16 @@
 import * as constants from './constants';
 import * as actions from './my-balance';
 
-describe('My Balance Action Creators', () => {
+describe('Balance Action Creators', () => {
   it('creates an action to set the balance', () => {
     const balance = 0;
     const expectedAction = {type: constants.SET_BALANCE, balance };
     expect(actions.setBalance(balance)).toEqual(expectedAction);
+  })
+
+  it('creates an action to deposit into the balance', () => {
+    const deposit = 1000;
+    const expectedAction = {type: constants.DEPOSIT, deposit };
+    expect(actions.deposit(deposit)).toEqual(expectedAction);
   })
 })
--- src/reducers/my-balance.test.js
+++ src/reducers/my-balance.test.js
@@ -1,11 +1,19 @@
 import balanceReducer from './my-balance';
 import * as constants from '../actions/constants';
 
-describe('Balance reducer', () => {
+describe('Balance Reducer', () => {
   it('sets a balance', () => {
     const balance = 10;
 
     expect(balanceReducer(undefined, {type: constants.SET_BALANCE, balance}))
       .toEqual(balance);
+  })
+
+  it('deposits into the balance', () => {
+    const deposit = 1000;
+    const startingBalance = 100;
+
+    expect(balanceReducer(startingBalance, {type: constants.DEPOSIT, deposit}))
+      .toEqual(startingBalance + deposit);
   })
 })
