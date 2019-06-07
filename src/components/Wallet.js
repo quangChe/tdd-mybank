12feb327d40c391fd9dbe0b61440db7babd9478e
quangChe <quang.n.che@gmail.com>
2019-06-06 23:17:16 -0700

    Completed a deposit method within Wallet component to pass test for dispatching a deposit

--- src/components/Wallet.js
+++ src/components/Wallet.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { balance } from '../actions/my-balance';
 
 export class Wallet extends React.Component {
   state = {
@@ -10,16 +11,23 @@ export class Wallet extends React.Component {
     this.setState({ amount: parseInt(e.target.value, 10) });
   }
 
+  deposit = () => {
+    this.props.deposit(this.state.amount);
+  }
+
   render() {
     const { balance } = this.props;
     
     return (
       <div>
-        <h3 className="balance">Wallet balance: { balance }</h3>
+        <h3 className='balance'>Wallet balance: { balance }</h3>
         <br/>
         <input 
           onChange={this.changeAmount}
-          className="wallet-input"/>
+          className='wallet-input'/>
+        <button
+          className='deposit-btn'
+          onClick={this.deposit}>Deposit</button>
       </div>
     )
   }
