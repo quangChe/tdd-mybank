48940f7a393a5311dd111447dba26b5df2032612
quangChe <quang.n.che@gmail.com>
2019-06-08 14:48:57 -0700

    Cleaned up the bitcoing action creator test

--- src/actions/bitcoin.test.js
+++ src/actions/bitcoin.test.js
@@ -5,17 +5,17 @@ import fetchMock from 'fetch-mock';
 import { FETCH_BITCOIN } from './constants';
 import { fetchBitcoin } from './bitcoin';
 
-const createMockStore = configureMockStore([thunk]);
-const store = createMockStore({ bitcoin: {} });
-
-const mockResponse = { body: { bpi: 'bitcoin price index' }};
-
-fetchMock.get('https://api.coindesk.com/v1/bpi/currentprice.json', mockResponse);
-
-it('creates an async action to fetch the bitcoin', () => {
-  const expectedActions = [ { bitcoin: mockResponse.body, type: FETCH_BITCOIN }];
-
-  return store.dispatch(fetchBitcoin()).then(() => {
-    expect(store.getActions()).toEqual(expectedActions);
+describe('Bitcoin Action Creator', () => {
+  const createMockStore = configureMockStore([thunk]);
+  const store = createMockStore({ bitcoin: {} });
+  const mockResponse = { body: { bpi: 'bitcoin price index' }};
+  fetchMock.get('https://api.coindesk.com/v1/bpi/currentprice.json', mockResponse);
+  
+  it('creates an async action to fetch the bitcoin', () => {
+    const expectedActions = [ { bitcoin: mockResponse.body, type: FETCH_BITCOIN }];
+  
+    return store.dispatch(fetchBitcoin()).then(() => {
+      expect(store.getActions()).toEqual(expectedActions);
+    })
   })
 })
