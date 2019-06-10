9c3cc7f091e9d24d3235f7bb11ab3ab81712859a
quangChe <quang.n.che@gmail.com>
2019-06-09 20:28:42 -0700

    Rendering Loot Component
    Implemented a new test to check that the App component renders a
    connected Loot component. Then fulfilled this test by providing the
    Loot component inside of the App container component.

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import Wallet from './Wallet';
+import Loot from './Loot';
 
 class App extends React.Component {
   render () {
     return (
     <>
       <h2>My Bank</h2>
+      <hr/>
       <Wallet />
+      <hr/>
+      <Loot />
     </>
     );
   }
