a6de9d9beb844d51390a486c048267c9d688bac2
quangChe <quang.n.che@gmail.com>
2019-06-10 17:14:10 -0700

    Converted App to a stateless functional component

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,8 @@ import React from 'react';
 import Wallet from './Wallet';
 import Loot from './Loot';
 
-class App extends React.Component {
-  render () {
-    return (
+const App = () => {
+  return (
     <>
       <h2>My Bank</h2>
       <hr/>
@@ -12,8 +11,7 @@ class App extends React.Component {
       <hr/>
       <Loot />
     </>
-    );
-  }
+  );
 }
 
 export default App;
